71f78f74c140e6a17b851f40e162fd50966b606f
leeseewoo <leeseewoo@gmail.com>
2021-04-20 21:43:11 +0900

    03-4

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import React, { Component } from 'react';
 import TodaysPlan from './03/TodaysPlan';
 import PropComponent from './03/PropsComponent';
 
+import ChildComponent2 from './03/ChildComponent2';
+
 
 
 class App extends Component {
   render() {
     return (
-      <PropComponent
-        name="Do it React!"
+      <ChildComponent2
+        objValue={{ age: 20}}
+        requiredStringValue="문자"
       />
     )
   }
